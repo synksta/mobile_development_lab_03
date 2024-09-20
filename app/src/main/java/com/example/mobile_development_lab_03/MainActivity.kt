db4d9185203664ff4f6e5e90742712c495a5cbba
synksta <contriturbamentum@gmail.com>
2024-09-20 10:02:04 +0700

    Done

--- app/src/main/java/com/example/mobile_development_lab_03/MainActivity.kt
+++ app/src/main/java/com/example/mobile_development_lab_03/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.mobile_development_lab_03
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textLabel = findViewById<TextView>(R.id.text_label)
+        val textEdit = findViewById<EditText>(R.id.text_edit)
+
+        fun countNotDigitsInString(s: String = ""): Int {
+            var res : Int = 0
+            for (c in s) if (c.code !in 48..57) res += 1
+            return res
+        }
+
+        textEdit.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) { }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            @SuppressLint("SetTextI18n")
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int)
+            {
+                val str : String = s.toString()
+                textLabel.text = "В введенной строке ${countNotDigitsInString(str)} не цифр"
+            }
+        })
+
     }
 }
